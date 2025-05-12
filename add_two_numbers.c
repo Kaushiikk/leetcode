98038b04784f876cd30d3832939d6c51d752173e
kaushiik <131187033+Kaushiikk@users.noreply.github.com>
2025-05-13 04:12:40 +0530

    solution

---
+++ add_two_numbers.c
@@ -0,0 +1,59 @@
+// You are given two non-empty linked lists, l1 and l2, where each represents a non-negative integer.
+
+// The digits are stored in reverse order, e.g. the number 123 is represented as 3 -> 2 -> 1 -> in the linked list.
+
+// Each of the nodes contains a single digit. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+// Return the sum of the two numbers as a linked list.
+
+// Example 1:
+// Input: l1 = [1,2,3], l2 = [4,5,6]
+
+// Output: [5,7,9]
+
+// Explanation: 321 + 654 = 975.
+// Example 2:
+
+// Input: l1 = [9], l2 = [9]
+
+// Output: [8,1]
+// Constraints:
+
+// 1 <= l1.length, l2.length <= 100.
+// 0 <= Node.val <= 9
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* cur = dummy;
+
+        int carry = 0;
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int v1 = (l1 != nullptr) ? l1->val : 0;
+            int v2 = (l2 != nullptr) ? l2->val : 0;
+
+            int val = v1 + v2 + carry;
+            carry = val / 10;
+            val = val % 10;
+            cur->next = new ListNode(val);
+
+            cur = cur->next;
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+        ListNode* res = dummy->next;
+        delete dummy;
+        return res;
+    }
+};
